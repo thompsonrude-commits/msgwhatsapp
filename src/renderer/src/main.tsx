95d59e6b3fe65dfcac2437a53111e0b80adf93ad
thompsonrude-commits <thompsonrude@googlemail.com>
2026-08-07 12:16:11 +0400

    v1.1.5: fix white screen

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,11 @@
 import './index.css'
 
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { ErrorBoundary } from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ErrorBoundary>
     <App />
-  </StrictMode>
+  </ErrorBoundary>
 )
